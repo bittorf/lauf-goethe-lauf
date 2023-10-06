51bf45635c438751f24f444e11b1d7dd2d0d5b17
Bastian Bittorf <bb@npl.de>
2023-10-06 20:43:42 +0200

    hintergrundbild+impressum

---
+++ README.md
@@ -0,0 +1,7 @@
+# Ein Experiment!
+
+...mit cloudflare pages
+
+### TODO
+* convert -verbose -density 300 -trim IN.pdf -quality 100  -flatten -sharpen 0x1.0 OUT.jpg
+
--- build.sh
+++ build.sh
@@ -13,12 +13,48 @@ echo "##################"
 
 mkdir -p dest
 
+cp -v flyer-crop-32cols-1920wide.webp dest/1.webp
+
 {
 echo "<html>"
-echo "<head><title>Lauf Goethe!</title></head>"
+
+echo "<head><title>Lauf Goethe!</title>"
+echo "<style>"
+echo "p {"
+echo "  background-image: url('1.webp');"
+echo "}"
+echo "</style>"
+echo "</head>"
+
 echo "<body bgcolor=white>"
-echo "Hier entsteht bald etwas...Geduld!"
+echo "<div class=container>"
+echo "<details>"
+echo "<summary>Impressum &amp; Kontakt</summary>"
+echo "<div>"
+
+cat <<EOF
+<pre>
+Veranstalter: Dr. Barbara Köllner, David Bauer
+
+Anschrift:    Dr. Barbara Köllner
+              Neuer Herrenweg 10
+              99428 Weimar
+
+Telefon:      015906768155
+
+E-Mail:       info@lauf-goethe-lauf.de
+
+ORGA-Team:    Dr. Barbara Köllner, David Bauer, Dr. Hendrik Schröter
+<pre>
+EOF
+
+echo "</div>"
+echo "</details"
+echo "</div>"
+
 echo "<body></html>"
+
 } >"dest/index.html"
 
+echo "siehe $PWD/dest"
 true
