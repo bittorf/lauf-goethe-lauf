03124e811fe3b1165cdd9d5076e82271c2380b25
Bastian Bittorf <bb@npl.de>
2023-10-06 19:40:55 +0200

    bald geht es los

--- build.sh
+++ build.sh
@@ -1,13 +1,24 @@
 #!/bin/sh
 
+echo "##################"
 head /proc/cpuinfo
 head /proc/meminfo
 ip a
+ifconfig
+cat /etc/resolv.conf
 cat /etc/*release*
 curl ifconfig.io
 id
+echo "##################"
 
 mkdir -p dest
-echo "<html><head><title>$( date )::mytitle></title></head><body bgcolor=green>mybody</body></html>" >"dest/index.html"
-true
 
+{
+echo "<html>"
+echo "<head><title>Lauf Goethe!</title></head>"
+echo "<body bgcolor=white>"
+echo "Hier entsteht bald etwas...Geduld!"
+echo "<body></html>"
+} >"dest/index.html"
+
+true
