4a02486d653574c9f55a1c227e23a231f39ba707
Bastian Bittorf <bb@npl.de>
2024-03-25 17:56:52 +0100

    change 'Home' button to an ARROW

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-#https://arup.dev/blog/2024/jekyll-cloudflare-pages-imagemagick/
+# https://arup.dev/blog/2024/jekyll-cloudflare-pages-imagemagick/
+# https://github.com/asdf-vm/asdf-plugins
 asdf plugin add imagemagick && asdf install imagemagick 7.1.1-29 && asdf global imagemagick 7.1.1-29
 
 test -d dest && rm -fR dest
